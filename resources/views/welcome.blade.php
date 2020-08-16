a6a2cff9942f1aeef4123660fd9b9ff1baa360e1
Souhei.Konishi <reepoo0528@outlook.jp>
2020-08-16 10:04:57 +0000

    timeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,8 @@
     @if (Auth::check())
         <div class="row">
             <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        {{-- 認証済みユーザーのメールアドレスをもとにGravatarを取得して表示 --}}
-                        <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
-                    </div>
-                </div>
+                {{-- ユーザ情報 --}}
+                @include('users.card')
             </aside>
             <div class="col-sm-8">
                 {{-- 投稿フォーム --}}
@@ -25,8 +18,8 @@
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to the Microposts</h1>
-                {{-- ユーザー登録ページへのリンク --}}
-                {!! link_to_route('signup.get' , 'Sign up now!' , [] , ['class' => 'btn btn-lg btn-primary']) !!}
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
     @endif
